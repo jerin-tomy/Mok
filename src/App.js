4c5fc3ec0af0f7a0ed80c3cff1b51942adb1d688
punteek <prateeksingh1@hotmail.com>
2018-11-10 13:59:54 -0800

    created user form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import UsernameForm from './components/UsernameForm';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUsername: '',
+    }
+    this.onUsernameSumbitted = this.onUsernameSubmitted.bind(this);
+  }
+
+  onUsernameSubmitted(username){
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username}),
+    }).then(response => {
+      this.setState({currentUsername: username});
+    }).catch(error => console.log('error', error));
+  }
+
+
   render() {
-    return <h1>Chatly</h1>
+    return <UsernameForm onSumbit={this.onUsernameSubmitted} />
   }
 }
 
